3a2bc4ae165060232e6a33eaf2e4acd8d2d1b54e
Andrew Mort <amort@andrew.cmu.edu>
2015-05-02 16:32:22 -0400

    Continuing comb testing

--- et_project5/test.sh
+++ et_project5/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ARG_DESIGNSOURCE=../verilog/s9234_comb.v
+ARG_DESIGNSOURCE=../verilog/s9234_comball.v
 ARG_WORKDIR=comb_dir
 TEMP_FILE=temp
 LOG_DIR=$ARG_WORKDIR/logs
@@ -17,7 +17,8 @@ echo "echo Building Test Mode" >> $TEMP_FILE
 echo "build_testmode TESTMODE=FULLSCAN WORKDIR=$ARG_WORKDIR > $LOG_DIR/build_testmode.out" >> $TEMP_FILE
 
 echo "echo Building Fault Model" >> $TEMP_FILE
-echo "build_faultmodel includeignore=no cellnamefaultrules=yes includedynamic=no faultpath=$PFAULT_DIR precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/build_faultmodel.out" >> $TEMP_FILE  
+#echo "build_faultmodel includeignore=no cellnamefaultrules=yes includedynamic=no faultpath=$PFAULT_DIR precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/build_faultmodel.out" >> $TEMP_FILE  
+echo "build_faultmodel includeignore=no includedynamic=no precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/build_faultmodel.out" >> $TEMP_FILE  
 
 echo "echo Verifying Test Structures" >> $TEMP_FILE
 echo "verify_test_structures WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN > $LOG_DIR/verfiy_test_structures.out" >> $TEMP_FILE
@@ -27,6 +28,10 @@ echo "create_logic_tests WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN EXPERIMENT=exp_c
 
 echo "echo Writing Vectors" >> $TEMP_FILE
 echo "write_vectors WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN language=stil inexperiment=exp_comb > $LOG_DIR/write_vectors.out" >> $TEMP_FILE
+
+echo "echo Reporting Faults" >> $TEMP_FILE
+echo "report_faults faulttype=static reportcircuit=yes faultstatus=redundant experiment=exp_comb testmode=FULLSCAN inputcommitted=no workdir=$ARG_WORKDIR > $LOG_DIR/report_faults.out" >> $TEMP_FILE
+#echo "report_faults faultstatus=untested,redundant experiment=exp_comb testmode=FULLSCAN inputcommitted=no workdir=$ARG_WORKDIR > $LOG_DIR/report_faults.out" >> $TEMP_FILE
 
 # ----------- Source Test Tools ------------
 source ../cet_source/tools_et
