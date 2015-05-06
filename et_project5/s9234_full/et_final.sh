c585559eb3b99ef4937c00fcf512e0f52e2868e0
Andrew Mort <amort@andrew.cmu.edu>
2015-05-05 21:35:13 -0400

    updated changes

--- et_project5/s9234_full/et_final.sh
+++ et_project5/s9234_full/et_final.sh
@@ -22,14 +22,14 @@ echo "echo Building Test Mode" >> $TEMP_FILE
 echo "build_testmode TESTMODE=FULLSCAN assignfile=$PINASSIGN WORKDIR=$ARG_WORKDIR > $LOG_DIR/2_build_testmode.out" >> $TEMP_FILE
 
 echo "echo Building Fault Model" >> $TEMP_FILE
-echo "build_faultmodel includeignore=no cellnamefaultrules=yes includedynamic=no faultpath=$PFAULT_DIR precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/3_build_faultmodel.out" >> $TEMP_FILE  
-#echo "build_faultmodel includeignore=no includedynamic=no precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/build_faultmodel.out" >> $TEMP_FILE  
+#echo "build_faultmodel includeignore=no cellnamefaultrules=yes includedynamic=no faultpath=$PFAULT_DIR precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/3_build_faultmodel.out" >> $TEMP_FILE  
+echo "build_faultmodel includeignore=no includedynamic=no  precollapsed=no workdir=$ARG_WORKDIR ignoreblackbox=yes cellfaults=no > $LOG_DIR/3_build_faultmodel.out" >> $TEMP_FILE  
 
 echo "echo Verifying Test Structures" >> $TEMP_FILE
-echo "verify_test_structures WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN > $LOG_DIR/3_verfiy_test_structures.out" >> $TEMP_FILE
+echo "verify_test_structures WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN > $LOG_DIR/4_verfiy_test_structures.out" >> $TEMP_FILE
 
 echo "echo Simulating Vectors" >> $TEMP_FILE
-echo "simulate_vectors WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN EXPERIMENT=exp_scan faulttype=static importfile=$INPUTVECTOR > $LOG_DIR/4_simulate_vectors.out" >> $TEMP_FILE
+echo "simulate_vectors WORKDIR=$ARG_WORKDIR TESTMODE=FULLSCAN EXPERIMENT=exp_scan faulttype=static simulation=gp importfile=$INPUTVECTOR > $LOG_DIR/5_simulate_vectors.out" >> $TEMP_FILE
 
 
 # ----------- Source Test Tools ------------
